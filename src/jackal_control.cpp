14e3fdf55617e1dba90e509da748f9559e9911a1
dprandle <dprandle@alaska.edu>
2022-10-28 19:06:44 -0800

    JS Working to control jackal

--- src/jackal_control.cpp
+++ src/jackal_control.cpp
@@ -27,10 +27,13 @@
 #include "Urho3D/Resource/JSONFile.h"
 #include "Urho3D/Resource/JSONValue.h"
 #include "Urho3D/Resource/ResourceCache.h"
+#include "Urho3D/UI/BorderImage.h"
+#include "Urho3D/UI/UIEvents.h"
 #include "Urho3D/UI/Button.h"
 #include "Urho3D/UI/UIElement.h"
 #include "Urho3D/UI/Sprite.h"
 
+#include <cstring>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -47,9 +50,9 @@
 const int SERVER_PORT = 8080;
 #include <emscripten/emscripten.h>
 #else
-const int SERVER_PORT = 3000;
+const int SERVER_PORT = 4000;
 #endif
-const char *SERVER_IP = "127.0.0.1";
+const char *SERVER_IP = "192.168.1.103";
 
 int main(int argc, char **argv)
 {
@@ -83,6 +86,102 @@ intern void setup_global_keys(jackal_control_ctxt *ictxt)
     ictxt->input_map.name = "global";
 }
 
+intern vec2 handle_jostick_move(const ivec2 &cur_mpos, joystick_panel * jspanel, float dev_pixel_ratio_inv)
+{
+    float max_r = jspanel->outer_ring->GetMaxOffset().x_ / 3.0f;
+    vec2 dir_vec = vec2{cur_mpos.x_, cur_mpos.y_} - jspanel->cached_mouse_pos;
+    auto l = dir_vec.Length();
+    if (l > max_r)
+        dir_vec = dir_vec * (max_r / l);
+    ivec2 new_pos = jspanel->cached_js_pos + ivec2(dir_vec.x_, dir_vec.y_);
+    jspanel->js->SetPosition(new_pos);
+    return dir_vec * (-1.0f / max_r);
+}
+
+intern void joystick_panel_init(velocity_info * write_vel, joystick_panel * jsp, urho::Context* uctxt, double dev_pixel_ratio_inv)
+{
+    // Get default style
+    urho::ResourceCache *cache = uctxt->GetSubsystem<urho::ResourceCache>();
+    auto *xmlFile = cache->GetResource<urho::XMLFile>("UI/jackal_style.xml");
+    urho::UI *usi = uctxt->GetSubsystem<urho::UI>();
+    urho::UIElement *root = usi->GetRoot();
+
+
+    jsp->frame = new urho::BorderImage(uctxt);
+    root->AddChild(jsp->frame);
+    jsp->frame->SetColor({0.2f, 0.2f, 0.2f, 0.7f});
+    jsp->frame->SetEnableAnchor(true);
+    jsp->frame->SetMinAnchor(0.0f, 0.7f);
+    jsp->frame->SetMaxAnchor(1.0f, 1.0f);
+
+    jsp->outer_ring = new urho::BorderImage(uctxt);
+    jsp->frame->AddChild(jsp->outer_ring);
+    jsp->outer_ring->SetStyle("JoystickBorder", xmlFile);
+    jsp->outer_ring->SetEnableAnchor(true);
+    jsp->outer_ring->SetMinAnchor(0.5f, 0.5f);
+    jsp->outer_ring->SetMaxAnchor(0.5f, 0.5f);
+
+    ivec2 offset = jsp->outer_ring->GetImageRect().Size();
+    offset.x_ *= dev_pixel_ratio_inv;
+    offset.y_ *= dev_pixel_ratio_inv;
+
+    jsp->outer_ring->SetMaxOffset(offset);
+    jsp->outer_ring->SetPivot(0.5f, 0.5f);
+    
+    jsp->js = new urho::Button(uctxt);
+    jsp->outer_ring->AddChild(jsp->js);
+    jsp->js->SetStyle("Joystick", xmlFile);
+    jsp->js->SetEnableAnchor(true);
+    jsp->js->SetMinOffset({0, 0});
+
+    offset = jsp->js->GetImageRect().Size();
+    offset.x_ *= dev_pixel_ratio_inv;
+    offset.y_ *= dev_pixel_ratio_inv;
+
+    jsp->js->SetMaxOffset(offset);
+    jsp->js->SetMinAnchor(0.5f, 0.5f);
+    jsp->js->SetMaxAnchor(0.5f, 0.5f);
+    jsp->js->SetPivot(0.5f, 0.5f);
+
+    jsp->js->SubscribeToEvent(urho::E_DRAGMOVE, [jsp, dev_pixel_ratio_inv, write_vel](urho::StringHash type, urho::VariantMap& ev_data) {
+        using namespace urho::DragMove;
+        auto elem = (urho::UIElement*)ev_data[P_ELEMENT].GetPtr();
+        if (elem == jsp->js)
+        {
+            auto dvec = handle_jostick_move({ev_data[P_X].GetInt(), ev_data[P_Y].GetInt()}, jsp, dev_pixel_ratio_inv);
+            write_vel->angular = dvec.x_;
+            write_vel->linear = dvec.y_;
+        }
+    });
+
+    jsp->js->SubscribeToEvent(urho::E_DRAGEND, [jsp, write_vel](urho::StringHash type, urho::VariantMap& ev_data) {
+        using namespace urho::DragEnd;
+        auto elem = (urho::UIElement*)ev_data[P_ELEMENT].GetPtr();
+        if (elem == jsp->js)
+        {
+            jsp->js->SetEnableAnchor(true);
+            jsp->cached_js_pos = {};
+            jsp->cached_mouse_pos = {};
+            *write_vel = {};
+        }
+    });
+
+    jsp->js->SubscribeToEvent(urho::E_DRAGBEGIN, [jsp, dev_pixel_ratio_inv](urho::StringHash type, urho::VariantMap& ev_data) {
+        using namespace urho::DragBegin;
+        auto elem = (urho::UIElement*)ev_data[P_ELEMENT].GetPtr();
+        if (elem == jsp->js)
+        {
+            ivec2 pos = jsp->js->GetPosition();
+            jsp->cached_js_pos = pos;
+            ivec2 cmp;
+            SDL_GetMouseState(&cmp.x_, &cmp.y_);
+            jsp->cached_mouse_pos = vec2{cmp.x_, cmp.y_} * dev_pixel_ratio_inv;
+            jsp->js->SetEnableAnchor(false);
+        }
+    });
+
+}
+
 intern void create_visuals(jackal_control_ctxt *ctxt)
 {
     auto uctxt = ctxt->urho_ctxt;
@@ -90,36 +189,9 @@ intern void create_visuals(jackal_control_ctxt *ctxt)
     auto graphics = uctxt->GetSubsystem<urho::Graphics>();
     graphics->SetWindowTitle("Jackal Control");
 
-    // Get default style
-    urho::ResourceCache *cache = uctxt->GetSubsystem<urho::ResourceCache>();
-    auto *xmlFile = cache->GetResource<urho::XMLFile>("UI/jackal_style.xml");
-    urho::UI *usi = uctxt->GetSubsystem<urho::UI>();
-    urho::UIElement *root = usi->GetRoot();
-
-    // auto btn = new urho::Button(uctxt);
-    // root->AddChild(btn);
-    // btn->SetStyleAuto(xmlFile);
-    // btn->SetEnableAnchor(true);
-    // btn->SetMinAnchor(0.1, 0.25);
-    // btn->SetMaxAnchor(0.75, 0.75);
-
-    auto joystick = new urho::Button(uctxt);
-    root->AddChild(joystick);
-    joystick->SetStyle("Joystick", xmlFile);
-    //joystick->SetPosition(vec2(500,500));
-    joystick->SetEnableAnchor(true);
-    int offset = 132;
-    joystick->SetMinOffset({0, 0});
-    joystick->SetMaxOffset({offset, offset});
-    joystick->SetMinAnchor(0.5f, 0.75f);
-    joystick->SetMaxAnchor(0.5f, 0.75f);
-    joystick->SetPivot(0.5f, 0.5f);
-    //joystick->SetSize(1000,1000);
-    //joystick->SetFullImageRect();
-
+    joystick_panel_init(&ctxt->vel_cmd.vinfo, &ctxt->js_panel, ctxt->urho_ctxt, ctxt->dev_pixel_ratio_inv);
 
     ctxt->scene = new urho::Scene(uctxt);
-
     ctxt->scene->CreateComponent<urho::DebugRenderer>();
     ctxt->scene->CreateComponent<urho::Octree>();
 
@@ -135,7 +207,7 @@ intern void create_visuals(jackal_control_ctxt *ctxt)
     vp->SetDrawDebug(true);
     rnd->SetViewport(0, vp);
     auto zn = rnd->GetDefaultZone();
-    zn->SetFogColor({0.7, 0.7, 0.7, 1.0});
+    zn->SetFogColor({0.0, 0.0, 0.0, 1.0});
 
     // Create a directional light
     urho::Node *light_node = ctxt->scene->CreateChild("dirlight");
@@ -145,6 +217,8 @@ intern void create_visuals(jackal_control_ctxt *ctxt)
     light->SetColor(Color(1.0f, 1.0f, 1.0f));
     light->SetSpecularIntensity(5.0f);
     light->SetBrightness(1.0f);
+
+    auto dpi = graphics->GetDisplayDPI();
 }
 
 void jctrl_alloc(jackal_control_ctxt *ctxt, urho::Context *urho_ctxt)
@@ -157,15 +231,21 @@ void jctrl_alloc(jackal_control_ctxt *ctxt, urho::Context *urho_ctxt)
 
 int socket_connect()
 {
-    struct pollfd sckt_fd;
+    struct pollfd sckt_fd[1];
 
-    sckt_fd.fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sckt_fd.fd == -1)
+    sckt_fd[0].fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sckt_fd[0].fd == -1)
     {
         elog("Failed to create socket");
         return 0;
     }
-    fcntl(sckt_fd.fd, F_SETFL, O_NONBLOCK);
+    else
+    {
+        ilog("Created socket with fd %d", sckt_fd[0].fd);
+    }
+    
+    fcntl(sckt_fd[0].fd, F_SETFL, O_NONBLOCK);
+    sckt_fd[0].events = POLLOUT;
 
     sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
@@ -179,46 +259,56 @@ int socket_connect()
         return -1;
     }
 
-    if (connect(sckt_fd.fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0 && errno != EINPROGRESS)
+    if (connect(sckt_fd[0].fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0 && errno != EINPROGRESS)
     {
-        elog("Failed connecting to server at %s on port %d - resulting fd %d - error %s", SERVER_IP, SERVER_PORT, sckt_fd.fd, strerror(errno));
-        return -1;
-    }
-
-    #ifndef __EMSCRIPTEN__
-    int pret = poll(&sckt_fd, 1, 10000);
-    if (pret == 0)
-    {
-        elog("Poll timed out connecting to server %s on port %d", SERVER_IP, SERVER_PORT);
-        return -1;
-    }
-    else if (pret == -1)
-    {
-        elog("Failed poll on trying to connect to server at %s on port %d - resulting fd %d - error %s", SERVER_IP, SERVER_PORT, sckt_fd.fd, strerror(errno));
+        elog("Failed connecting to server at %s on port %d - resulting fd %d - error %s", SERVER_IP, SERVER_PORT, sckt_fd[0].fd, strerror(errno));
+        close(sckt_fd[0].fd);
         return -1;
     }
     else
     {
-        if (test_flags(sckt_fd.revents, POLLERR))
+        ilog("Connected to server at %s on port %d", SERVER_IP, SERVER_PORT);
+    }
+
+    #ifndef __EMSCRIPTEN__
+    int pret = poll(sckt_fd, 1, -1);
+    if (pret == 0)
+    {
+        elog("Poll timed out connecting to server %s on port %d for fd %d", SERVER_IP, SERVER_PORT, sckt_fd[0].fd);
+        close(sckt_fd[0].fd);
+        return -1;
+    }
+    else if (pret == -1)
+    {
+        elog("Failed poll on trying to connect to server at %s on port %d - resulting fd %d - error %s", SERVER_IP, SERVER_PORT, sckt_fd[0].fd, strerror(errno));
+        close(sckt_fd[0].fd);
+        return -1;
+    }
+    else
+    {
+        if (test_flags(sckt_fd[0].revents, POLLERR))
         {
-            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLERR", SERVER_IP, SERVER_PORT, sckt_fd.fd);
+            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLERR", SERVER_IP, SERVER_PORT, sckt_fd[0].fd);
+            close(sckt_fd[0].fd);
             return -1;
         }
-        else if (test_flags(sckt_fd.revents, POLLHUP))
+        else if (test_flags(sckt_fd[0].revents, POLLHUP))
         {
-            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLHUP", SERVER_IP, SERVER_PORT, sckt_fd.fd);
+            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLHUP", SERVER_IP, SERVER_PORT, sckt_fd[0].fd);
+            close(sckt_fd[0].fd);
             return -1;
         }
-        else if (test_flags(sckt_fd.revents, POLLNVAL))
+        else if (test_flags(sckt_fd[0].revents, POLLNVAL))
         {
-            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLNVAL", SERVER_IP, SERVER_PORT, sckt_fd.fd);
+            elog("Failed connecting to server at %s on port %d - resulting fd %d - POLLNVAL", SERVER_IP, SERVER_PORT, sckt_fd[0].fd);
+            close(sckt_fd[0].fd);
             return -1;
         }
     }
     #endif
 
-    ilog("Successfully connected to server at %s on port %d - resulting fd %d", SERVER_IP, SERVER_PORT, sckt_fd.fd);
-    return sckt_fd.fd;
+    ilog("Successfully connected to server at %s on port %d - resulting fd %d", SERVER_IP, SERVER_PORT, sckt_fd[0].fd);
+    return sckt_fd[0].fd;
 }
 
 void jctrl_free(jackal_control_ctxt *ctxt)
@@ -237,8 +327,8 @@ bool jctrl_init(jackal_control_ctxt *ctxt)
     engine_params[urho::EP_FRAME_LIMITER] = false;
     engine_params[urho::EP_LOG_NAME] = log_loc;
     engine_params[urho::EP_FULL_SCREEN] = false;
-    engine_params[urho::EP_WINDOW_WIDTH] = 1440;
-    engine_params[urho::EP_WINDOW_HEIGHT] = 2960;
+    engine_params[urho::EP_WINDOW_WIDTH] = 1080;
+    engine_params[urho::EP_WINDOW_HEIGHT] = 2400;
 #if !defined(__EMSCRIPTEN__)
     engine_params[urho::EP_HIGH_DPI] = true;
     engine_params[urho::EP_WINDOW_RESIZABLE] = true;
@@ -246,11 +336,17 @@ bool jctrl_init(jackal_control_ctxt *ctxt)
     if (!ctxt->urho_engine->Initialize(engine_params))
         return false;
 
+    #if defined(__EMSCRIPTEN__)
+    ctxt->dev_pixel_ratio_inv = 1.0 / emscripten_get_device_pixel_ratio();
+    #endif
+
     create_visuals(ctxt);
 
     input_init(&ctxt->input_dispatch);
     setup_global_keys(ctxt);
     ctxt->input_dispatch.context_stack.push_back(&ctxt->input_map);
+
+    ilog("Device pixel ratio inverse: %f", ctxt->dev_pixel_ratio_inv);
 
     ctxt->socket_fd = socket_connect();
 
@@ -280,6 +376,10 @@ void jctrl_exec(jackal_control_ctxt *ctxt)
 
 void jctrl_terminate(jackal_control_ctxt *ctxt)
 {
+    if (ctxt->socket_fd > 0)
+    {
+        close(ctxt->socket_fd);
+    }
 }
 
 void jctrl_run_frame(jackal_control_ctxt *ctxt)
@@ -293,7 +393,7 @@ void jctrl_run_frame(jackal_control_ctxt *ctxt)
     if (ctxt->socket_fd > 0)
     {
         int rd_cnt = read(ctxt->socket_fd, read_buf, BUF_SIZE);
-        if (rd_cnt >= 0)
+        if (rd_cnt >1)
         {
             ilog("Read %d bytes from socket", rd_cnt);
             for (int i = 0; i < rd_cnt; ++i)
@@ -303,7 +403,22 @@ void jctrl_run_frame(jackal_control_ctxt *ctxt)
         }
         else if (rd_cnt != 0 && errno != EAGAIN && errno != EWOULDBLOCK)
         {
-            elog("Got read error %d", strerror(errno));
+            elog("Got read error %s", strerror(errno));
         }
+
+        if (!ctxt->js_panel.js->GetEnableAnchor())
+        {
+            sizet bytes = sizeof(command_velocity);
+            sizet bwritten = write(ctxt->socket_fd, &ctxt->vel_cmd, sizeof(command_velocity));
+            if (bwritten == -1)
+            {
+                elog("Got write error %d", strerror(errno));
+            }
+            else if (bytes != bwritten)
+            {
+                wlog("Bytes written was only %d when tried to write %d", bwritten, bytes);
+            }
+        }
+
     }
 }
--- src/jackal_control.h
+++ src/jackal_control.h
@@ -4,6 +4,7 @@
 #include <Urho3D/Core/Object.h>
 #include <Urho3D/Input/Input.h>
 #include <Urho3D/Math/Vector3.h>
+#include <vector>
 
 #include "input.h"
 #include "ss_router.h"
@@ -14,11 +15,32 @@ namespace Urho3D
 class Node;
 class Scene;
 class Sprite;
+class Button;
 }
 
 
 class Input_Map;
 struct Input_Context;
+
+struct command_packet_header
+{
+    u8 command_type;
+    u8 future;
+    u8 future2;
+    u8 future3;
+};
+
+struct velocity_info
+{
+    float linear {0.0};
+    float angular {0.0};
+};
+
+struct command_velocity
+{
+    command_packet_header header {0};
+    velocity_info vinfo {};
+};
 
 struct jctrl_uevent_handlers : urho::Object
 {
@@ -30,6 +52,15 @@ struct jctrl_uevent_handlers : urho::Object
     bool debug_draw {false};
 };
 
+struct joystick_panel
+{
+    urho::BorderImage * frame {};
+    urho::BorderImage * outer_ring {};
+    urho::Button * js {};
+    ivec2 cached_js_pos {};
+    vec2 cached_mouse_pos {};
+};
+
 struct jackal_control_ctxt
 {
     urho::Context* urho_ctxt {};
@@ -38,12 +69,16 @@ struct jackal_control_ctxt
     urho::Scene * scene {};    
     urho::Node * cam_node {};
 
+    joystick_panel js_panel {};
     input_context_stack input_dispatch {};
     input_keymap input_map {};
 
     jctrl_uevent_handlers *event_handler;
     ss_router router;
+
+    command_velocity vel_cmd;
     int socket_fd {0};
+    float dev_pixel_ratio_inv {1.0};
 };
 
 void jctrl_alloc(jackal_control_ctxt * ctxt, urho::Context* urho_ctxt);
